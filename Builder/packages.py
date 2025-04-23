d7c4ba303e9e3fa1fa20919a6b56d79afd75f06d
Itai <81989649+itai215@users.noreply.github.com>
2025-04-23 15:00:02 +0300

    Update packages.py
    Removed firefox, and moved a few packages to AUR_PACKAGES

--- Builder/packages.py
+++ Builder/packages.py
@@ -1,19 +1,18 @@
 
 
 BASE_PACKAGES = [
-    "tumbler", "ffmpegthumbnailer", "lsd", "alacritty", "bat", "brightnessctl", "calc",
-    "automake", "blueman", "bluez", "bluez-utils", "dunst", "fakeroot", "feh", "firefox",
+    "tumbler", "ffmpegthumbnailer", "alacritty", "bat", "brightnessctl", "calc",
+    "automake", "blueman", "bluez", "bluez-utils", "dunst", "fakeroot", "feh",
     "fish", "dpkg", "gcc", "gedit", "git", "gnu-netcat", "htop", "btop", "nano", "lxappearance",
     "mat2", "mpd", "mpv", "thunar", "ncmpcpp", "neofetch", "network-manager-applet", "nitrogen",
     "pamixer", "papirus-icon-theme", "pavucontrol", "polybar", "autoconf", "mpc", "pulseaudio",
-    "pulseaudio-alsa", "python-pyalsa", "ranger", "redshift", "reflector", "rofi", "rofi-calc", "calcurse",
-    "rofi-emoji", "scrot", "sudo", "slop", "tree", "unrar", "zip", "unzip", "uthash", "xarchiver",
+    "pulseaudio-alsa", "python-pyalsa", "ranger", "redshift", "reflector", "rofi", "calcurse",
+    "scrot", "sudo", "slop", "tree", "unrar", "zip", "unzip", "uthash", "xarchiver",
     "xfce4-power-manager", "xfce4-settings", "xorg-xbacklight", "zathura", "zathura-djvu", "zathura-pdf-mupdf",
     "cmake", "clang", "gzip", "imagemagick", "make", "openssh", "pulseaudio-bluetooth", "shellcheck",
-    "vlc", "usbutils", "picom", "networkmanager-openvpn", "alsa-plugins", "alsa-tools", "alsa-utils", "ffmpeg",
-    "p7zip", "gparted", "sshfs", "openvpn", "xclip", "gpick", "wget", "ueberzug", "netctl", "light", "libreoffice",
-    "breeze", "vulkan-intel", "intel-ucode", "ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd", "ttf-fira-code",
-    "ttf-iosevka-nerd"
+    "vlc", "usbutils", "picom", "networkmanager-openvpn", "networkamanager-dinit", "alsa-utils-dinit", "alsa-utils", "ffmpeg",
+    "p7zip", "gparted", "sshfs", "openvpn", "xclip", "gpick", "wget", "ueberzug", "netctl", "light",
+    "breeze", "vulkan-intel", "intel-ucode"
 ]
 
 DEV_PACKAGES = [
@@ -21,11 +20,12 @@ DEV_PACKAGES = [
     "filezilla", "ghex", "chromium", "keepassxc", "audacity", "gufw", "python-pywal",
     "bleachbit", "veracrypt", "homebank", "gtkhash", "gnome-firmware", "touche", "dconf-editor",
     "neovim", "obs-studio", "telegram-desktop", "tmux", "youtube-dl", "code", "cowsay",
-    "deluge-gtk", "flameshot", "sqlitebrowser", "obsidian", "python-pip", "bpython", "ipython", "cloc",
+    "deluge-gtk", "flameshot", "sqlitebrowser", "obsidian", "python-pip", "bpython", "ipython", "cloc", 
 ]
 
 AUR_PACKAGES = [
-    "cava", "i3lock-color", "ptpython"
+    "cava", "i3lock-color", "ptpython", "ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd", "ttf-fira-code",
+    "ttf-iosevka-nerd", "lsd", "rofi-calc", "rofi-emoji", "alsa-tools", "alsa-plugins"
 ]
 
 GNOME_OFFICIAL_TOOLS = [

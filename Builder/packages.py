34c3f0a229dd1b965282c5e3f2a2ac2f3f9dc9b0
Itai <81989649+itai215@users.noreply.github.com>
2025-04-24 20:57:43 +0300

    Update packages.py
    Moved packages to AUR packages that doesn't exist in artix repo

--- Builder/packages.py
+++ Builder/packages.py
@@ -4,14 +4,14 @@ BASE_PACKAGES = [
     "tumbler", "ffmpegthumbnailer", "alacritty", "bat", "brightnessctl", "calc",
     "automake", "blueman", "bluez", "bluez-utils", "dunst", "fakeroot", "feh",
     "fish", "dpkg", "gcc", "gedit", "git", "gnu-netcat", "htop", "btop", "nano", "lxappearance",
-    "mat2", "mpd", "mpv", "thunar", "ncmpcpp", "neofetch", "network-manager-applet", "nitrogen",
-    "pamixer", "papirus-icon-theme", "pavucontrol", "polybar", "autoconf", "mpc", "pulseaudio",
-    "pulseaudio-alsa", "python-pyalsa", "ranger", "redshift", "reflector", "rofi", "calcurse",
+    "mat2", "mpd", "mpv", "thunar", "ncmpcpp", "neofetch", "network-manager-applet",
+    "papirus-icon-theme", "pavucontrol", "polybar", "autoconf", "mpc", "pulseaudio",
+    "pulseaudio-alsa", "python-pyalsa", "ranger", "redshift", "rofi", "calcurse",
     "scrot", "sudo", "slop", "tree", "unrar", "zip", "unzip", "uthash", "xarchiver",
     "xfce4-power-manager", "xfce4-settings", "xorg-xbacklight", "zathura", "zathura-djvu", "zathura-pdf-mupdf",
     "cmake", "clang", "gzip", "imagemagick", "make", "openssh", "pulseaudio-bluetooth", "shellcheck",
-    "vlc", "usbutils", "picom", "networkmanager-openvpn", "networkamanager-dinit", "alsa-utils-dinit", "alsa-utils", "ffmpeg",
-    "p7zip", "gparted", "sshfs", "openvpn", "xclip", "gpick", "wget", "ueberzug", "netctl", "light",
+    "vlc", "usbutils", "picom", "networkmanager-openvpn", "networkmanager-dinit", "alsa-utils-dinit", "alsa-utils", "ffmpeg",
+    "p7zip", "gparted", "sshfs", "openvpn", "xclip", "wget", "light",
     "breeze", "vulkan-intel", "intel-ucode"
 ]
 
@@ -25,7 +25,8 @@ DEV_PACKAGES = [
 
 AUR_PACKAGES = [
     "cava", "i3lock-color", "ptpython", "ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd", "ttf-fira-code",
-    "ttf-iosevka-nerd", "lsd", "rofi-calc", "rofi-emoji", "alsa-tools", "alsa-plugins"
+    "ttf-iosevka-nerd", "lsd", "rofi-calc", "rofi-emoji", "alsa-tools", "alsa-plugins", "gpick", "nitrogen", "pamixer", "reflector", "netctl", 
+    "python-pyalsa", "ueberzug"
 ]
 
 GNOME_OFFICIAL_TOOLS = [
